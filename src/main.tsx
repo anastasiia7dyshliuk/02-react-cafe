b898111aae2d6248c1ac4272e05e3bbcbe0c2cda
Anastasiia Dyshliuk <a.r.dyshliuk@gmail.com>
2026-09-07 00:07:14 +0300

    fix: correct modern-normalize import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "modern-normalize";
-import App from "./components/App/App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'modern-normalize/modern-normalize.css';
+import App from './components/App/App';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
